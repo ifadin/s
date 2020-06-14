3e6cc15c8042f9a7b21c42988a37050363f5a7ac
Iaroslav Fadin <iaroslav.fadin@zalando.de>
2020-06-14 19:07:46 +0200

    Added parallel requests

--- csgo/bs/updater.py
+++ csgo/bs/updater.py
@@ -4,8 +4,9 @@ import time
 from typing import List, Dict, Set, Optional
 from urllib.parse import urlparse, parse_qs
 
-import grequests
-from requests import Response, PreparedRequest
+from requests import Response, PreparedRequest, Request
+from requests.auth import AuthBase
+from requests_toolbelt import threaded
 from tqdm import tqdm
 
 from csgo.bs.token import TokenProvider
@@ -15,17 +16,16 @@ from csgo.type.item import ItemCollection
 from csgo.type.price import PriceEntry, ItemPrices, ItemSales
 
 
-class BSAuthAsyncRequest(grequests.AsyncRequest):
+class BSAuth(AuthBase):
 
-    def __init__(self, method, url, token_provider: TokenProvider, **kwargs):
-        super().__init__(method, url, **kwargs)
+    def __init__(self, token_provider: TokenProvider) -> None:
         self.token_provider = token_provider
 
-    def send(self, **kwargs):
+    def __call__(self, r: Request):
         api_key = self.token_provider.get_api_key()
         token = self.token_provider.get_token()
-        self.url = self.url + f'&api_key={api_key}&code={token}'
-        return super().send(**kwargs)
+        r.url = r.url + f'&api_key={api_key}&code={token}'
+        return r
 
 
 class BSUpdater(Updater):
@@ -43,17 +43,26 @@ class BSUpdater(Updater):
 
     def request_prices(self, items: Dict[str, float], per_page: int = 480) -> ItemPrices:
         prices = {}
-        processing_requests = [
-            self.request_inventory_on_sale(market_name, self.token_provider, ref_price, per_page=per_page)
-            for market_name, ref_price in items.items()
-        ]
-        with tqdm(total=len(items)) as pbar:
+        auth = BSAuth(self.token_provider)
+        processing_requests = [{
+            'method': 'GET',
+            'url': self.request_inventory_on_sale(item_name, ref_price, per_page=per_page),
+            'auth': auth
+        } for item_name, ref_price in items.items() if item_name.lower()[0] in {'m'}]
+
+        with tqdm(total=len(processing_requests)) as pbar:
             while processing_requests:
                 tqdm.write(f'Got batch of {len(processing_requests)} request(s) to process')
                 additional_requests = []
-                for res in grequests.imap(processing_requests, size=10):
-                    success = self.check_request(res, additional_requests, self.token_provider)
+                rate_limited = False
 
+                responses_generator, exceptions_generator = threaded.map(processing_requests)
+                for res in responses_generator:
+                    if not rate_limited and res.status_code == 429:
+                        time.sleep(3)
+                        rate_limited = True
+
+                    success = self.check_request(res, additional_requests, auth)
                     if success:
                         original_req: PreparedRequest = res.request
                         items = self.get_prices_from_response(res)
@@ -62,14 +71,18 @@ class BSUpdater(Updater):
 
                         page = res.json().get('data', {}).get('page')
                         item_name = parse_qs(urlparse(original_req.url).query)['market_hash_name'][0]
-                        max_price = parse_qs(urlparse(original_req.url).query)['max_price'][0]
-                        tqdm.write(f'Updated {item_name} {max_price} with {len(items)} items - '
+                        tqdm.write(f' - Updated {item_name} with {len(items)} items - '
                                    f'{page} ({res.elapsed.seconds}s)')
 
                         if page and len(items) == per_page:
-                            self.add_next_page_request(res, additional_requests, self.token_provider, page)
+                            next_page = page + 1
+                            tqdm.write(f'   Scheduling page {next_page} for {item_name}')
+                            self.add_next_page_request(res, additional_requests, next_page, auth)
                         else:
                             pbar.update(1)
+
+                for exc in exceptions_generator:
+                    print(f'ERROR : {exc}')
 
                 processing_requests = additional_requests
 
@@ -77,15 +90,24 @@ class BSUpdater(Updater):
 
     def request_sales(self, items: Set[str]) -> ItemSales:
         sales = {}
-        processing_requests = [self.request_price_history(market_name, self.token_provider) for market_name in items]
+        auth = BSAuth(self.token_provider)
+        processing_requests = [{
+            'method': 'GET', 'url': self.request_price_history(market_name), 'auth': auth
+        } for market_name in items]
 
-        with tqdm(total=len(items)) as pbar:
+        with tqdm(total=len(processing_requests)) as pbar:
             while processing_requests:
                 tqdm.write(f'Got batch of {len(processing_requests)} request(s) to process')
                 additional_requests = []
-                for res in grequests.imap(processing_requests, size=5):
-                    success = self.check_request(res, additional_requests, self.token_provider)
+                rate_limited = False
 
+                responses_generator, exceptions_generator = threaded.map(processing_requests)
+                for res in responses_generator:
+                    if not rate_limited and res.status_code == 429:
+                        time.sleep(3)
+                        rate_limited = True
+
+                    success = self.check_request(res, additional_requests, auth)
                     if success:
                         original_req: PreparedRequest = res.request
                         sale_price = self.get_sale_price_from_response(res)
@@ -95,19 +117,20 @@ class BSUpdater(Updater):
                             pbar.update(1)
                         tqdm.write(f'Updated {item_name} with {sale_price} ({res.elapsed.seconds}s)')
 
+                for exc in exceptions_generator:
+                    print(f'ERROR : {exc}')
+
                 processing_requests = additional_requests
 
         return sales
 
     @classmethod
-    def check_request(cls, res: Response, requests_queue, token_provider: TokenProvider):
+    def check_request(cls, res: Response, requests_queue, auth: AuthBase):
         if res.status_code in [429, 401]:
             item_name = parse_qs(urlparse(res.request.url).query)['market_hash_name'][0]
             tqdm.write(f'[WARN] rescheduling {item_name}')
 
-            if res.status_code == 429:
-                time.sleep(3)
-            requests_queue.append(BSAuthAsyncRequest('GET', res.request.url, token_provider))
+            requests_queue.append({'method': 'GET', 'url': res.request.url, 'auth': auth})
             return False
         else:
             res.raise_for_status()
@@ -116,22 +139,22 @@ class BSUpdater(Updater):
         return True
 
     @classmethod
-    def add_next_page_request(cls, res: Response, requests_queue, token_provider: TokenProvider, page: int):
-        next_page = page + 1
-        requests_queue.append(BSAuthAsyncRequest('GET', res.request.url + f'&page={next_page}', token_provider))
+    def add_next_page_request(cls, res: Response, requests_queue, next_page: int, auth: AuthBase):
+        requests_queue.append({
+            'method': 'GET', 'url': res.request.url + f'&page={next_page}', 'auth': auth
+        })
 
     @classmethod
-    def request_inventory_on_sale(cls, market_name: str, token_provider: TokenProvider, ref_price: float,
-                                  page: int = 1, per_page=480) -> grequests.AsyncRequest:
+    def request_inventory_on_sale(cls, market_name: str, ref_price: float,
+                                  page: int = 1, per_page=480) -> str:
         max_price = f'{ref_price:.2f}' if ref_price else ''
-        return BSAuthAsyncRequest('GET',
-                                  f'{cls.prices_url}/?app_id=730&market_hash_name={market_name}'
-                                  f'&is_souvenir=-1&max_price={max_price}&sort_by=price&order=asc'
-                                  f'&page={page}&per_page={per_page}', token_provider)
+        return (f'{cls.prices_url}/?app_id=730&market_hash_name={market_name}'
+                f'&is_souvenir=-1&max_price={max_price}&sort_by=price&order=asc'
+                f'&page={page}&per_page={per_page}')
 
     @classmethod
-    def request_price_history(cls, market_name: str, token_provider: TokenProvider) -> grequests.AsyncRequest:
-        return BSAuthAsyncRequest('GET', f'{cls.sales_url}/?app_id=730&market_hash_name={market_name}', token_provider)
+    def request_price_history(cls, market_name: str) -> str:
+        return f'{cls.sales_url}/?app_id=730&market_hash_name={market_name}'
 
     @classmethod
     def check_api_status(cls, res: Response):
@@ -141,8 +164,7 @@ class BSUpdater(Updater):
 
     @classmethod
     def get_prices_from_response(cls, res: Response) -> List[PriceEntry]:
-        return [PriceEntry(i['market_hash_name'], float(i['price']), float(i['float_value']), i['item_rarity'],
-                           item_id=i['item_id'])
+        return [PriceEntry(i['market_hash_name'], float(i['price']), float(i['float_value']), item_id=i['item_id'])
                 for i in res.json().get('data', {}).get('items', []) if i.get('price') and i.get('float_value')]
 
     @classmethod
